0fa942f2b2a02d4c7079e64a882d6135e3ea248c
soolstafir <radiukpavlo@gmail.com>
2018-02-01 12:32:46 +0200

    Start the project.

---
+++ download_images.py
@@ -0,0 +1,62 @@
+import argparse
+from imutils import paths
+import requests
+import cv2
+import os
+
+# construct the argument parse and parse the arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', '--urls', required=True)
+parser.add_argument('-o', '--output', required=True)
+args = vars(parser.parse_args())
+
+# grab the list of URLs from the input file, then initialize the
+# total number of images downloaded thus far
+rows = open(args["urls"]).read().strip().split("\n")
+total = 0
+
+# loop the URLs
+for url in rows:
+    try:
+        # try to download the image
+        image_download = requests.get(url, timeout=60)
+
+        # save the image to disk
+        image_save = os.path.sep.join([args['output'], '{0}.jpg'.format(
+            str(total).zfill(8))])
+        image_open = open(image_save, 'wb')
+        image_open.write(image_download.content)
+        image_open.close()
+
+        # update the counter
+        print("[INFO] downloaded: {}".format(image_save))
+        total += 1
+
+    # handle if any exceptions are thrown during the downloaded process
+    except:
+        print("[INFO] error downloading {0}...skipping".format(image_save))
+
+# loop over the image paths we just downloaded
+for imagePath in paths.list_images(args["output"]):
+    # initialize if the image should be deleted or not
+    delete = False
+
+    # try to load the image
+    try:
+        image = cv2.imread(imagePath)
+
+        # if the image is `None` then we could not properly load it
+        # from disk, so delete it
+        if image is None:
+            delete = True
+
+    # if OpenCV cannot load the image then the image is likely
+    # corrupt so we should delete it
+    except:
+        print("Except")
+        delete = True
+
+    # check to see if the image should be deleted
+    if delete:
+        print("[INFO] deleting {}".format(imagePath))
+        os.remove(imagePath)
